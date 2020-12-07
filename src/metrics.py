Fix excess entropy metric calculation

src/metrics.py
@@ -1,13 +1,41 @@
 import numpy as np
+import nltk
+from typing import Union
 
 
-def excess_entropy_slow(text: str, single_H, pair_H):
+def calculate_entropy(distribution: Union[list, np.ndarray]) -> float:
+    EPS = 1e-9
+    p = np.array(distribution)
+    return (-p * np.log(p + EPS) - (1 - p) * np.log(1 - p + EPS)).sum()
+
+
+def get_H_single(F_pos: nltk.FreqDist, F_single: nltk.FreqDist):
+    def H_single(i, x_i):
+        p = F_single[(i, x_i)] / F_pos[i]
+        return calculate_entropy([p, 1 - p])
+    return H_single
+
+
+def get_H_pair(F_pos: nltk.FreqDist, F_single: nltk.FreqDist, F_pair: nltk.FreqDist, F_last: nltk.FreqDist):
+    def H_pair(i, x_prv, x_cur):
+        T = F_pos[i]
+        c11 = F_pair[(i, (x_prv, x_cur))]
+        c01 = F_single[(i, x_cur)] - c11
+        c10 = F_single[(i - 1, x_prv)] - c11 - F_last[(i - 1, x_prv)]
+        c00 = T - c11 - c01 - c10
+        assert c00 >= 0
+        return calculate_entropy(np.array([c00, c01, c10, c11], dtype=float) / T) - \
+               calculate_entropy(np.array([c10 + c11, c00 + c01]) / T)
+    return H_pair
+
+
+def excess_entropy_slow(text: str, H_single, H_pair):
     """
     Calculates excess entropy of given string in O(n^2) time complexity in a straightforward way
 
     :param text: an input tokenized string
-    :param single_H: a function that calculates H(i, x_i)
-    :param pair_H: a function that calculates H(x_i | x_{i-1}) = H(i, x_i, x_{i-1})
+    :param H_single: a function that calculates H(i, x_i)
+    :param H_pair: a function that calculates H(x_i | x_{i-1}) = H(i, x_{i-1}, x_i)
     :return: a float value which is equal to excess entropy of given input string
     """
 
@@ -17,9 +45,9 @@ def excess_entropy_slow(text: str, single_H, pair_H):
         H = 0
         for i in range(l, r):
             if i == l:
-                H += single_H(i, text[i])
+                H += H_single(i, text[i])
             else:  # i > l
-                H += pair_H(i, text[i], text[i - 1])
+                H += H_pair(i, text[i - 1], text[i])
         return H
 
     EE = 0
@@ -30,20 +58,20 @@ def excess_entropy_slow(text: str, single_H, pair_H):
     return EE - (n - 1) * calculate_excess_entropy_on_range(0, n)
 
 
-def excess_entropy_fast(text: str, single_H, pair_H):
+def excess_entropy_fast(text: str, H_single, H_pair):
     """
     Calculates excess entropy of given string in O(n) time complexity
 
     :param text: an input tokenized string
-    :param single_H: a function that calculates H(i, x_i)
-    :param pair_H: a function that calculates H(x_i | x_{i-1}) = H(i, x_i, x_{i-1})
+    :param H_single: a function that calculates H(i, x_i)
+    :param H_pair: a function that calculates H(x_i | x_{i-1}) = H(i, x_{i-1}, x_i)
     :return: a float value which is equal to excess entropy of given input string
     """
 
     n = len(text)
     EE = 0
     for i in range(n - 1):
-        EE += single_H(i + 1, text[i + 1]) - pair_H(i + 1, text[i + 1], text[i])
+        EE += H_single(i + 1, text[i + 1]) - H_pair(i + 1, text[i], text[i + 1])
     return EE
 
 