1e019882328dc95798fd7fbdcb2f780b9bc19814
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-12-11 01:13:15 +0300

    Add tfds mode int stat collectings

--- src/collect_statistics.py
+++ src/collect_statistics.py
@@ -3,14 +3,18 @@ import pickle
 
 import nltk
 import sentencepiece as spm
-from tqdm import tqdm
+import tensorflow_datasets as tfds
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--spm', type=str, help='Path to the file with spm tokenizer')
-    parser.add_argument('--corpus', type=str, help='Path to the corpus file which should be processes')
+    parser.add_argument('--corpus', type=str, help='Path to the corpus file which should be processes '
+                                                   'or name of tfds')
     parser.add_argument('--save-prefix', type=str, help='Prefix of file to be saved after corpus processing')
+    parser.add_argument('--mode', type=str, choices=['file', 'tfds'], required=True)
+    parser.add_argument('--dump', type=str, help='Path to the dump directory, where tfds is stored')
+    parser.add_argument('--logging-period', type=int, default=1000)
     return parser.parse_args()
 
 
@@ -19,6 +23,19 @@ def get_from_file_generator(path: str):
         for line in file:
             line = line.strip()
             yield line
+
+
+def get_tfds_generator(args):
+    dataset = tfds.load(
+        name=args.corpus,
+        data_dir=args.dump,
+        with_info=False,
+        split=tfds.Split.TRAIN,
+        shuffle_files=False,
+        download=False
+    )
+    for item in dataset:
+        yield str(item['text'].numpy().decode('utf-8')).strip()
 
 
 if __name__ == '__main__':
@@ -32,8 +49,12 @@ if __name__ == '__main__':
     F_pair = nltk.FreqDist()
     F_last = nltk.FreqDist()
 
-    for line in tqdm(get_from_file_generator(args.corpus)):
-        line = line.strip()
+    generator = {
+        'file': get_from_file_generator(args.corpus),
+        'tfds': get_tfds_generator(args)
+    }[args.mode]
+
+    for i, line in enumerate(generator):
         x = sp.encode_as_ids(line)
         n = len(x)
         if n == 0:
@@ -48,6 +69,12 @@ if __name__ == '__main__':
         F_single += add_single
         F_pair += add_pair
         F_last[(n - 1, x[n - 1])] += 1
+
+        if i % args.logging_period == 0:
+            print('\n==========\n')
+            print(f'{i} iterations, |F_pos| = {len(F_pos)}, |F_single| = {len(F_single)}, '
+                  f'|F_pair| = {len(F_pair)}, |F_last| = {len(F_last)}')
+            print(f'sample = {line}')
 
     with \
         open(f'{args.save_prefix}_pos', 'wb') as out_pos, \
