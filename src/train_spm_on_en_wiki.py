489f5e1356c2c2aadbd1bddda956888ea4853995
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-12-10 23:47:29 +0300

    Add script for training spm using tfds wiki40b

---
+++ src/train_spm_on_en_wiki.py
@@ -0,0 +1,56 @@
+import argparse
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import io
+import sentencepiece as spm
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, help='Code of the dataset in tfds', default='wiki40b/en')
+    parser.add_argument('--vocab-size', type=int, help='The size of vocabulary', default=10000)
+    parser.add_argument('--save', type=str, help='Path to the file where spm model will be saved')
+    parser.add_argument('--download', type=bool, default=False)
+    parser.add_argument('--model-type', type=str, default='bpe')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # sp = spm.SentencePieceProcessor()
+    # sp.load('test.model')
+    # print(sp.vocab_size())
+    # for i in range(sp.vocab_size()):
+    #     print(i, sp.id_to_piece(i))
+
+    DATA_DIR = 'dump'
+
+    dataset, dataset_info = tfds.load(
+        name=args.dataset,
+        data_dir=DATA_DIR,
+        with_info=True,
+        split=tfds.Split.TRAIN,
+        shuffle_files=False,
+        download=args.download
+    )
+
+    def article_to_text(text):
+        return text.numpy().decode('utf-8')
+
+    dataset_text = dataset.map(
+        lambda article: tf.py_function(func=article_to_text, inp=[article['text']], Tout=tf.string)
+    )
+
+    model = io.BytesIO()
+
+    spm.SentencePieceTrainer.train(
+        sentence_iterator=dataset_text.as_numpy_iterator(),
+        model_writer=model,
+        vocab_size=args.vocab_size,
+        model_type=args.model_type
+    )
+
+    with open(args.save, 'wb') as f:
+        f.write(model.getvalue())
