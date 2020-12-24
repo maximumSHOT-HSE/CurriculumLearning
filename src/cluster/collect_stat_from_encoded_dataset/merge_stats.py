c727ed4a29dd7c82a7a675e9770fce2217070471
Maxim Surkov <surkovmax007@mail.ru>
2020-12-24 16:58:29 +0300

    Add merge stats script

---
+++ src/cluster/collect_stat_from_encoded_dataset/merge_stats.py
@@ -0,0 +1,91 @@
+from transformers import BertForSequenceClassification
+from tokenizers import normalizers, pre_tokenizers, Tokenizer, models, trainers
+import datasets
+from transformers import BertTokenizer, BertTokenizerFast, AutoTokenizer, AutoModelForSequenceClassification
+from transformers import BertForSequenceClassification, Trainer, TrainingArguments
+from datasets import load_dataset, load_metric
+import numpy as np
+import torch
+from collections import Counter
+from tqdm import tqdm
+import pickle
+import argparse
+import json
+from pathlib import Path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prefix', type=str, help='Prefix of directories names from which partial statistics will be retrieved for merging', required=True)
+    parser.add_argument('--save', type=str, help='Path to the EMPTY directory, where statistics will be saved', required=True)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    roots = list(sorted(Path().rglob(f'{args.prefix}*')))
+
+    len_counter = Counter()
+    last_counter =  Counter()
+    single_counter = Counter()
+    pair_counter = Counter()
+
+    save_root = Path(args.save)
+
+    all_paths = {
+        'len_counter.bin': len_counter,
+        'last_counter.bin': last_counter,
+        'single_counter.bin': single_counter,
+        'pair_counter.bin': pair_counter
+    }
+
+    config = None
+
+    for root in tqdm(roots):
+        with open(root / 'config.json', 'r') as f:
+            c = json.load(f)
+            if config is None:
+                config = c
+            else:
+                assert config['tokenizer'] == c['tokenizer']
+                assert config['dataset'] == c['dataset']
+                assert config['part'] == c['part']
+
+        assert config is not None
+
+        tmp_len_counter = Counter()
+        tmp_last_counter =  Counter()
+        tmp_single_counter = Counter()
+        tmp_pair_counter = Counter()
+
+        paths = {
+            'len_counter.bin': tmp_len_counter,
+            'last_counter.bin': tmp_last_counter,
+            'single_counter.bin': tmp_single_counter,
+            'pair_counter.bin': tmp_pair_counter
+        }
+
+        for path in paths.keys():
+            tmp_counter = paths[path]
+            all_counter = all_paths[path]
+            with open(root / path, 'rb') as f:
+                tmp_counter = pickle.load(f)
+                all_counter += tmp_counter
+
+    all_config = {
+        'tokenizer': config['tokenizer'],
+        'dataset': config['dataset'],
+        'part': config['part']
+    }
+
+    print(all_config)
+
+    for path, counter in all_paths.items():
+        print(path, len(counter))
+        with open(save_root / path, 'wb') as f:
+            pickle.dump(counter, f)
+
+    with open(save_root / 'config.json', 'w') as f:
+        json.dump(all_config, f)
+
---
+++ src/cluster/collect_stat_from_encoded_dataset/run_merge_validation_stats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#SBATCH --time=10:00:00
+#SBATCH --gpus=0
+#SBATCH --cpus-per-task=4
+
+module add singularity hpcx/hpcx-ompi
+singularity exec --nv ~/containers/container.sif python3 merge_stats.py --prefix save_stat/save_stat_validation --save save_stat/save_all_validation
