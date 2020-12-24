27311f0f64f9c4f1dfd9d9c5fc741c837dd6d332
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-12-25 00:35:48 +0300

    Add curriculum hyperbole sampler

--- src/curriculum_utils.py
+++ src/curriculum_utils.py
@@ -64,6 +64,58 @@ class CurriculumSampler(Sampler):
         return len(self.indices)
 
 
+class CurriculumSamplerHyperbole(Sampler):
+    r"""Samples elements in the following way:
+        1. All dataset is splitted into n_bins bins (last bin may have smaller size than others)
+        2. Sampler assumes that will be n_see * (n_bins + 2 * window_width - 2) epochs, whee n_see = the number of times we want to give samples into model
+        3. At the (q * (n_bins + 2 * window_width - 2) + mod)-th epoch sampler gives the probabilities for each bin: for each 0 <= i <= window_width,
+            let t = (q - window_width + 1) be the center of the current window, then
+            weight of the i-th bins will be 1 / (|i - t| + 1)^ro
+            then sampler will sample indices from given bins with that weights.
+            In othe words, we will consider distribution with some center, where located the biggest mass and mass linearly decreases both to the
+            right and to the left of the center. Center will move to the right every n_see epochs.
+        4. Notice that after n_see * (n_bins + 2 * window_width - 2) eachs bins will have equal
+            expected number (n_see) of times when index from i-th bin will be sampled
+    """
+
+    def __init__(
+        self,
+        data_source: Optional[Sized],
+        state: TrainerState,
+        n_bins: int,
+        window_width: int,
+        n_see: int,
+        ro: float
+    ):
+        super().__init__(data_source)
+        self.data_source = data_source
+        self.state = state
+        self.n_bins = n_bins
+        self.size = len(self.data_source)
+        self.window_width = window_width
+        self.n_see = n_see
+        self.bin_size = math.ceil(self.size / n_bins)
+        self.ro = ro
+
+        assert state.num_train_epochs == n_see * (n_bins + 2 * window_width - 2), "Should be consistent nmber of train epochs"
+
+        self.indices = self.build_indices()
+
+    def build_indices(self):
+        t = math.floor(self.state.epoch) // self.n_see - self.window_width + 1
+        p = 1 / (abs(np.arange(self.n_bins) - t) + 1) ** self.ro
+        p /= p.sum()
+        ids = np.random.choice(self.n_bins, self.bin_size, p=p) * self.bin_size + np.random.choice(self.bin_size, self.bin_size)
+        ids = ids[ids < self.size]
+        return ids
+
+    def __iter__(self):
+        yield from self.indices
+
+    def __len__(self):
+        return len(self.indices)
+
+
 class CurriculumTrainer(Trainer):
 
     def _get_train_sampler(self) -> Optional[torch.utils.data.sampler.Sampler]:
