6a7dd21a819452a131cb3ad8391cab3084830e34
Mosin Vladislav <mWX996951@china.huawei.com>
2021-03-03 02:42:42 +0300

    plotter can work with local files

--- src/plotter.py
+++ src/plotter.py
@@ -5,10 +5,34 @@ import subprocess
 from pathlib import Path
 from argparse import ArgumentParser
 import numpy as np
+from enum import Enum
+from shutil import copyfile
+import os
 
 from plotly.subplots import make_subplots
 
 LOG_DIRECTORY = "Logs"
+
+
+class PathType(Enum):
+    LOCAL = 0,
+    CLUSTER = 1
+
+    @staticmethod
+    def from_name(type_):
+        if type_ == 'local':
+            return PathType.LOCAL
+        elif type_ == 'cluster':
+            return PathType.CLUSTER
+        else:
+            raise
+
+
+class Experiment:
+    def __init__(self, path_type, path, experiment_group):
+        self.path_type = PathType.from_name(path_type)
+        self.path = path
+        self.experiment_group = experiment_group
 
 
 def load_config(path):
@@ -18,36 +42,29 @@ def load_config(path):
     return config
 
 
-def get_short_names(experiments):
-    splitted_experiments = []
-    for experiment in experiments:
-        splitted_experiments.append([path.split('/') for path in experiment])
-
-    for i in range(len(splitted_experiments[0][0])):
-        if splitted_experiments[0][0][i] != splitted_experiments[1][0][i]:
-            return [[f'{experiment[j][i]}_{j}' for j in range(len(experiment))] for experiment in splitted_experiments]
-
-
 def load_from_cluster(local_path, cluster_path):
     subprocess.run(['scp', '-rP', '2222', f'aomelchenko@cluster.hpc.hse.ru:{cluster_path}', local_path])
 
 
-def get_last_checkpoint(experiment_path):
-    checkpoints = subprocess.Popen(['ssh', '-p', '2222', f'aomelchenko@cluster.hpc.hse.ru',
-                                    f'ls {experiment_path}'],
-                                   stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split('\n')
+def get_last_checkpoint(experiment):
+    if experiment.path_type == PathType.CLUSTER:
+        checkpoints = subprocess.Popen(['ssh', '-p', '2222', f'aomelchenko@cluster.hpc.hse.ru', f'ls {experiment.path}'],
+                                       stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split('\n')
+    else:
+        checkpoints = os.listdir(experiment.path)
 
     return sorted(checkpoints, key=lambda checkpoint: int(checkpoint.split('-')[1]))[-1]
 
 
 def load_all_experiments(all_experiments):
-    all_short_names = get_short_names(all_experiments)
-    for short_names, experiment_paths in zip(all_short_names, all_experiments):
-        for short_name, experiment_path in zip(short_names, experiment_paths):
-            cluster_path = f'{experiment_path}/{get_last_checkpoint(experiment_path)}/trainer_state.json'
-            local_path = f'{LOG_DIRECTORY}/{short_name}.json'
-            print(cluster_path)
-            load_from_cluster(local_path=local_path, cluster_path=cluster_path)
+    for experiment_group in all_experiments.keys():
+        for i, experiment in enumerate(all_experiments[experiment_group]):
+            local_path = f'{LOG_DIRECTORY}/{experiment.experiment_group}{i}.json'
+            previous_path = f'{experiment.path}/{get_last_checkpoint(experiment)}/trainer_state.json'
+            if experiment.path_type == PathType.CLUSTER:
+                load_from_cluster(local_path=local_path, cluster_path=previous_path)
+            else:
+                copyfile(previous_path, local_path)
 
 
 def get_loss_history(history, metric):
@@ -62,12 +79,12 @@ def get_loss_history(history, metric):
     return epochs, metrics
 
 
-def get_experiment_data(experiment_names, title):
+def get_experiment_data(experiments, title):
     all_losses = []
     all_epochs = []
 
-    for experiment_name in experiment_names:
-        path_to_config = f'{LOG_DIRECTORY}/{experiment_name}.json'
+    for i, experiment in enumerate(experiments):
+        path_to_config = f'{LOG_DIRECTORY}/{experiment.experiment_group}{i}.json'
         history = load_config(path_to_config)['log_history']
         epochs, losses = get_loss_history(history, title)
         all_losses.append(losses)
@@ -87,29 +104,29 @@ FILL_COLORS = ['rgba(31, 119, 180, 0.2)', 'rgba(255, 127, 14, 0.2)', 'rgba(44, 1
 
 
 def plot(experiments):
-    short_names = get_short_names(experiments)
-    plot_titles = ['eval_accuracy', 'eval_f1', 'eval_precision', 'eval_recall', 'eval_loss', 'loss']
+    plot_titles = ['eval_loss', 'loss'] #['eval_accuracy', 'eval_f1', 'eval_precision', 'eval_recall', 'eval_loss', 'loss']
 
     fig = make_subplots(rows=3, cols=2, subplot_titles=plot_titles)
 
-    for num, experiment in enumerate(short_names):
+    for num, experiment_group in enumerate(experiments.keys()):
         for i, title in enumerate(plot_titles):
-            epochs, values = get_experiment_data(experiment, title)
+            epochs, values = get_experiment_data(experiments[experiment_group], title)
             row = i // 2 + 1
             col = i % 2 + 1
 
             means = values.mean(axis=0)
-            deviations = values.std(axis=0)
+            deviations = values.std(axis=0) + 0.01
 
             values_lower = (means - deviations).tolist()[::-1]
             values_upper = (means + deviations).tolist()
 
-            fig.add_trace(go.Scatter(x=epochs, y=means, name=experiment[0][:-2], legendgroup=experiment[0][:-2], showlegend=i == 0,
+            fig.add_trace(go.Scatter(x=epochs, y=means, name=experiment_group,
+                                     legendgroup=experiment_group, showlegend=i == 0,
                                      line=dict(color=COLORS[num])),
                           row=row, col=col)
             fig.add_trace(go.Scatter(x=epochs + epochs[::-1], y=values_upper + values_lower, fill='tozerox',
                                      fillcolor=FILL_COLORS[num], line=dict(color='rgba(255,255,255,0)'), opacity=0,
-                                     showlegend=False, legendgroup=experiment[0][:-2], name=experiment[0][:-2]),
+                                     showlegend=False, legendgroup=experiment_group, name=experiment_group),
                           row=row, col=col)
             fig.update_xaxes(title_text="steps", row=row, col=col)
             fig.update_yaxes(title_text=title, row=row, col=col)
@@ -135,21 +152,18 @@ def parse_config():
 
 
 def load_experiment_paths(config):
-    all_experiments = []
+    all_experiments = {}
     with open(config, 'r') as f:
-        single_experiments = []
-        for path in [line.strip() for line in f.readlines()]:
-            if path == "":
-                if len(single_experiments) > 0:
-                    all_experiments.append(single_experiments)
-                single_experiments = []
-            else:
-                single_experiments.append(path)
+        for experiment_string in [line.strip() for line in f.readlines()]:
+            path_type, path, experiment_group = experiment_string.split(' ')
+            experiment = Experiment(path_type, path, experiment_group)
 
-        if len(single_experiments) > 0:
-            all_experiments.append(single_experiments)
+            if experiment.experiment_group not in all_experiments:
+                all_experiments[experiment_group] = []
+            all_experiments[experiment_group].append(experiment)
 
     return all_experiments
+
 
 if __name__ == '__main__':
     show_results()
