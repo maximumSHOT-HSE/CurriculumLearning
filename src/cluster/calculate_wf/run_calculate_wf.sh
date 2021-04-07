a1bfb131a89ff3ffc4dad8148367ed2955d44d87
Maxim Surkov <surkovmax007@mail.ru>
2021-04-07 21:43:47 +0300

    add wf calculation

---
+++ src/cluster/calculate_wf/run_calculate_wf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#SBATCH --time=30-00:00:00
+#SBATCH --gpus=0
+#SBATCH --cpus-per-task=4
+
+DATASET=$1
+STATS=$2
+SAVE=$3
+
+module add singularity hpcx/hpcx-ompi
+singularity exec --nv ~/containers/container.sif python3 calculate_wf.py --dataset $DATASET --stats $STATS --save $SAVE
+
